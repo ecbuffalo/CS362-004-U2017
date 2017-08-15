3fc5d586befcf58d048e133ea6417f14d236de59
olivasd <olivasd@oregonstate.edu>
2017-08-14 20:06:06 -0700

    2 partition tests

--- projects/fjerstam/FinalProject/URLValidatorInCorrect/src/UrlValidatorTest.java
+++ projects/fjerstam/FinalProject/URLValidatorInCorrect/src/UrlValidatorTest.java
@@ -52,10 +52,37 @@ public class UrlValidatorTest extends TestCase {
    
    public void testYourFirstPartition()
    {
-	   
+       System.out.println("******First Partition*******");
+       UrlValidator urlVal = new UrlValidator(null, null, UrlValidator.ALLOW_ALL_SCHEMES);
+       String[] schemeValid = {"http://", "ftp://", "h3t://", ""};
+       String[] schemeInvalid = {"3ht://", "http:/", "http:",  "http/", "://"};
+
+       System.out.println("***Should be True***");
+       for(int i = 0; i < schemeValid.length; i++) {
+           System.out.println(urlVal.isValid(schemeValid[i] + "www.amazon.com"));
+       }
+       System.out.println("***Should be False***");
+       for(int i = 0; i < schemeInvalid.length; i++) {
+           System.out.println(urlVal.isValid(schemeInvalid[i] + "www.amazon.com"));
+       }
+
    }
    
    public void testYourSecondPartition(){
+       System.out.println("******Second Partition*******");
+       UrlValidator urlVal = new UrlValidator(null, null, UrlValidator.ALLOW_ALL_SCHEMES);
+       String[] urlValid = {"www.google.com", "go.com", "go.au", "0.0.0.0", "255.255.255.255", "255.com", "go.cc"};
+       String[] urlInvalid = {"256.256.256.256", "1.2.3.4.5", "1.2.3.4.", "1.2.3", ".1.2.3.4", "go.a", "go.a1a", "go.1aa", "aaa.", ".aaa", "aaa", ""};
+
+       System.out.println("***Should be True***");
+       for (int i = 0; i < urlValid.length; i++){
+           System.out.println(urlVal.isValid("http://" + urlValid[i]));
+       }
+
+       System.out.println("***Should be False***");
+       for (int i = 0; i < urlInvalid.length; i++){
+           System.out.println(urlVal.isValid("http://" + urlInvalid[i]));
+       }
 	   
    }
    
